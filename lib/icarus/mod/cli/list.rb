c68cb0c7ea536aaa930e455a0632732a27451866
Donovan C. Young <dyoung522@gmail.com>
2023-01-02 11:07:06 -0500

    Add subsorting by name to `list mods` command

--- lib/icarus/mod/cli/list.rb
+++ lib/icarus/mod/cli/list.rb
@@ -21,20 +21,22 @@ module Icarus
         desc "mods", "Displays data from 'mods'"
         method_option :sort, type: :string, default: "name", desc: "Sort by field (name, author, etc.) - defaults to 'name'"
         def mods
-          raise "Invalid sort field '#{options[:sort]}'" unless Icarus::Mod::Tools::Modinfo::HASHKEYS.include?(options[:sort].to_sym)
+          raise "Invalid sort field '#{options[:sort]}'" unless (Icarus::Mod::Tools::Modinfo::HASHKEYS + [:updated_at]).include?(options[:sort].to_sym)
 
           sort_field = options[:sort].to_sym
           header_format = "%-<name>50s %-<author>20s %-<version>10s %-<updated_at>20s"
-          header_format += " %<description>s" if verbose > 1
+          header_format += " %-<id>20s %<description>s" if verbose > 1
 
-          puts format(header_format, name: "NAME", author: "AUTHOR", version: "VERSION", updated_at: "LAST UPDATED", description: "DESCRIPTION") if verbose?
+          puts format(header_format, name: "NAME", author: "AUTHOR", version: "VERSION", updated_at: "LAST UPDATED", id: "ID", description: "DESCRIPTION") if verbose?
 
           mods = Firestore.new.list(:mods)
-          mods.sort_by { |m| m.send(sort_field) }.each do |mod|
-            data_format = "%-<name>50s %-<author>20s v%-<version>10s%-<updated_at>20s"
-            data_format += " %<description>s" if verbose > 1
 
-            puts format(data_format, mod.to_h.merge(updated_at: mod.updated_at.strftime("%Y-%m-%d %H:%M:%S")))
+          # Sort by field, optionally subsorting by name
+          (sort_field == :name ? mods.sort_by(&:name) : mods.sort_by { |mod| [mod.send(sort_field), mod.name] }).each do |mod|
+            data_format = "%-<name>50s %-<author>20s v%-<version>10s%-<updated_at>20s"
+            data_format += " %-<id>20s %<description>s" if verbose > 1
+
+            puts format(data_format, mod.to_h.merge(id: mod.id, updated_at: mod.updated_at.strftime("%Y-%m-%d %H:%M:%S")))
           end
 
           puts "Total: #{mods.count}" if verbose?
