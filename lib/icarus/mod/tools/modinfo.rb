db5620fafefd750196d6470f00e4000971adae1f
Donovan Young <dyoung522@gmail.com>
2023-02-04 22:37:46 +0000

    fix: Create fileType and fileURL along with files

--- lib/icarus/mod/tools/modinfo.rb
+++ lib/icarus/mod/tools/modinfo.rb
@@ -11,13 +11,17 @@ module Icarus
 
         def to_h
           db_hash = HASHKEYS.each_with_object({}) do |key, hash|
-            next if %i[fileType fileURL].include?(key.to_sym)
+            # next if %i[fileType fileURL].include?(key.to_sym)
             next if key == :long_description && @data[:long_description].nil?
 
             hash[key] = @data[key]
           end
 
           db_hash[:files] = { @data[:fileType].downcase.to_sym => @data[:fileURL] } if db_hash[:files].nil? && !@data[:fileType].nil?
+
+          # Remove this once all mods have been updated
+          db_hash[:fileType] = db_hash[:files].keys.first.to_s if db_hash[:fileType].nil? && !db_hash[:files].nil?
+          db_hash[:fileURL] = db_hash[:files].values.first if db_hash[:fileURL].nil? && !db_hash[:files].nil?
 
           db_hash
         end
